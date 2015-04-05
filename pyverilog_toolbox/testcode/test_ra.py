0f1b640eae60643156ec2fab81389b9538cc6b33
ryosuke fukatani <nannyakannya@gmail.com>
2015-04-05 14:16:31 +0900

    add new test

--- pyverilog_toolbox/testcode/test_ra.py
+++ pyverilog_toolbox/testcode/test_ra.py
@@ -39,5 +39,11 @@ class TestSequenceFunctions(unittest.TestCase):
                         'TOP.reg0[3]: set([(TOP.reg0, 3), (TOP.WRITE, 0), (TOP.WRITE_DATA, 1)])' +
                         'TOP.reg0[4]: set([(TOP.reg0, 4), (TOP.WRITE_DATA, 2), (TOP.WRITE, 0)])')
 
+    def test_split2(self):
+        ranalyzer = RegMapAnalyzer("regmap2.v", "setup.txt")
+        write_map, read_map = ranalyzer.getRegMaps()
+        self.assertEqual(str(write_map.map),
+                        "{0: {0: ('TOP.reg0', 3), 1: ('TOP.reg0', 4)}}")
+
 if __name__ == '__main__':
     unittest.main()
