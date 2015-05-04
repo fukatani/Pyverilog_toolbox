b43d6215f53a2402ae4d3f4a65412be933051fb3
ryosuke fukatani <nannyakannya@gmail.com>
2015-05-04 17:05:43 +0900

    modify __init__.py

--- pyverilog_toolbox/verify_tool/__init__.py
+++ pyverilog_toolbox/verify_tool/__init__.py
@@ -1,6 +1,8 @@
 import sys
 if sys.version_info[0] < 3:
     import bindlibrary
+    import dataflow_facade
     import regmap_analyzer
     import combloop_finder
-
+    import codeclone_finder
+    import cnt_analyzer
