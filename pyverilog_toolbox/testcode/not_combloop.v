01d613a0c571a0228c424205638d18a4f97e042f
ryosuke fukatani <nannyakannya@gmail.com>
2015-08-05 21:05:29 +0900

    modify testcode

--- pyverilog_toolbox/testcode/not_combloop.v
+++ pyverilog_toolbox/testcode/not_combloop.v
@@ -1,9 +1,9 @@
-module TOP(CLK, RST);
-  input CLK,RST;
+module TOP(CLK, RSTN);
+  input CLK,RSTN;
   reg reg1;
 
   always @(posedge CLK or negedge RSTN) begin
-    if(RST) begin
+    if(!RSTN) begin
       reg1 <= 0;
     end else begin
       reg1 <= reg1;
